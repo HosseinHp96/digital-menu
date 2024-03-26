0cc7ab04e70edf2e03e4f957d9fd0c1b70bc083f
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-03-26 20:05:10 +0330

    Initialise express app

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+//json
+app.use(express.json());
+
+//cors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-HEADERS", "Content-Type");
+  next();
+});
+
+export default app;
