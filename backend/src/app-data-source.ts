ad2e23e349feb9d0f17e9e64e7878af6c92a185c
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-03-28 19:04:51 +0330

    backend : Correct the data source to connect to the database

--- backend/src/app-data-source.ts
+++ backend/src/app-data-source.ts
@@ -1,16 +1,16 @@
+import "reflect-metadata";
 import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
-  port: 3306,
+  port: 5432,
   username: "postgres",
-  password: "postgres",
-  database: "postgres",
-  entities: ["entities/*.ts"],
+  password: "qwer1234",
+  database: "digital-menu-db",
+  entities: ["src/entities/**/*.ts"],
   logging: true,
   synchronize: true,
-  entitySkipConstructor: true,
 });
 
 export const initAppDS = () => {
