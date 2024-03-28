067f1b5102516d15bd60243d1f4c0be43412083b
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-03-28 16:34:05 +0330

    Combine the server file and the app file

--- backend/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./src/app";
-import { initAppDS } from "./src/app-data-source";
-
-// establish database connection
-initAppDS();
-
-const port = process.env.PORT || "3000";
-
-app.listen(port, () => console.log(`Sever is running on port ${port}`));
---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { initAppDS } from "./app-data-source";
+import { productRoute } from "./routes";
+
+// establish database connection
+initAppDS();
+
+const app: express.Application = express();
+
+// json
+app.use(express.json());
+
+// cors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-HEADERS", "Content-Type");
+  next();
+});
+
+// routes
+app.use("/api/products", productRoute);
+
+// port
+const port = process.env.PORT || "3000";
+
+// start server
+app.listen(port, () => console.log(`Sever is running on port ${port}`));
