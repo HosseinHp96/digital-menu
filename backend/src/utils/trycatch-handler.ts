fe60cc3ea9271a2da9501d3a49a2c589db9eea3a
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-04-06 14:05:38 +0330

    backend : Add 'next' arg to tryCatchHandler function

--- backend/src/utils/trycatch-handler.ts
+++ backend/src/utils/trycatch-handler.ts
@@ -1,10 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
 export const tryCatchHandler = (controller: Function) => {
-
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await controller(req, res);
+      await controller(req, res, next);
     } catch (err) {
       next(err);
     }
