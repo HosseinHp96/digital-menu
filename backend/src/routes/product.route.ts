0a544220f3a84d274e0a8f25763d4fbbd566363e
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-04-24 16:28:13 +0330

    backend : Add a route to create the product

--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { productController } from "../controllers";
-import { auth } from "../middlewares";
+import { auth, uploadImages } from "../middlewares";
 
 const router = Router();
 
 router.use(auth("admin"));
 router.get("/", productController.allProducts);
 router.get("/:id", productController.getProductByID);
+router.post("/add", uploadImages.single("image"), productController.addProduct);
 
 export { router as productRouter };
