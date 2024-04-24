03ca1965d5ff74c189def264eeb3708a26f3199d
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-04-24 16:25:14 +0330

    backend : Create a controller to add product

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { productServices } from "../services";
 import { tryCatchHandler } from "../utils";
+import fs from "fs";
 
 export const allProducts = tryCatchHandler(
   async (req: Request, res: Response) => {
@@ -17,3 +18,19 @@ export const getProductByID = tryCatchHandler(
     res.json(product);
   }
 );
+
+export const addProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    req.body.image = req.file?.path;
+
+    const product = await productServices.addProduct(req.body);
+    res.json(product);
+  } catch (err) {
+    if (req.body.image) fs.unlinkSync(req.body.image);
+    next(err);
+  }
+};
