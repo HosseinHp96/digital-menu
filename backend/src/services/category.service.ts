2b179064963a692f631fe5e815d5eb310e7e882b
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-04-28 21:48:45 +0330

    backend : Cteate all required services for the categoy entity

---
+++ backend/src/services/category.service.ts
@@ -0,0 +1,49 @@
+import CategoryDao from "../dao/category.dao";
+import { Category } from "../entities";
+import { AppError } from "../utils";
+
+export const allCategories = async () => {
+  return await CategoryDao.allCategories();
+};
+
+export const getCategoryByID = async (id: number) => {
+  return await CategoryDao.getCategory({ id });
+};
+
+export const addCategory = async (category: Category) => {
+  const status = await CategoryDao.getCategory({ title: category.title });
+
+  if (status)
+    throw new AppError({
+      message: "This category is currently available",
+      statusCode: 400,
+    });
+
+  return await CategoryDao.addCategory(category);
+};
+
+export const updateCategory = async (data: Category, id: number) => {
+  const category = await CategoryDao.getCategory({ id });
+
+  if (!category)
+    throw new AppError({
+      message: "There is no category with this ID",
+      statusCode: 400,
+    });
+
+  const result = await CategoryDao.updateCategory({ ...data, id });
+  return result;
+};
+
+export const removeCategory = async (id: number) => {
+  const category = await CategoryDao.getCategory({ id });
+
+  if (!category)
+    throw new AppError({
+      message: "There is no category with this ID",
+      statusCode: 400,
+    });
+
+  await CategoryDao.removeCategory(id);
+  return "Done";
+};
